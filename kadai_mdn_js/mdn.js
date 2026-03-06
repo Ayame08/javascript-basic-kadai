6cb48859800859743372fd06343b353600a43f9f
Ayame08 <snow4696@hotmail.co.jp>
2026-03-06 17:28:48 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -2,8 +2,9 @@
 const today = new Date();
 // 年、月、日をそれぞれ取得
 const year = today.getFullYear();
-const month = today.getMonth();
+const month = today.getMonth()+1;
 const day = today.getDate();
 
 // x年x月x日の形で表示
 console.log(year+'年'+month+'月'+day+'日');
+
